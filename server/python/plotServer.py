6f18817dc9e525c93a69123ffbd60214b89831a6
Daniel Grimes <danielgrimes0@gmail.com>
2016-02-21 12:04:41 -0500

    enums referenced in external file
    pop pop pop pop

--- server/python/plotServer.py
+++ server/python/plotServer.py
@@ -1,33 +1,6 @@
 import matplotlib.pyplot as plt
 import socket
-#import enums 
-
-class Command:
-	Idle = -1
-	Function = 0
-	Data = 1
-	Done = 2
-	Close = 3
-
-class Function:
-	Nothing = -1
-	Plot = 0
-	Figure = 1
-	SetupPlot = 2
-	Print = 3
-	Xlabel = 4
-	Ylabel = 5
-	Title = 6
-	Subplot = 7
-	Legend = 8
-	Hold = 9
-	Axis = 1
-	Grid = 1
-	Contour = 1
-	Colorbar = 1
-	Semilogx = 1
-	Semilogy = 15
-	Loglog = 16
+import enums 
 
 def Plot(data):
 	print('butts')
@@ -60,16 +33,16 @@ def serve():
 
 		currentCommand = data[0]
 		print('currentCommand = '+str(currentCommand))
-		if currentCommand == Command.Function: # function command
+		if currentCommand == enums.Command.Function: # function command
 			currentFunction = data[1]
 			currentPayload = int.from_bytes(data[2:-1], 'little', signed=False)
 			print('currentFunction = '+str(currentFunction))
 			print('currentPayload = '+str(currentPayload))
 
-		elif currentCommand == Command.Data: # data command
-			if currentFunction == Function.Plot: # plot function
+		elif currentCommand == enums.Command.Data: # data command
+			if currentFunction == enums.Function.Plot: # plot function
 				Plot(data)
-			elif currentFunction == Function.Figure: # figure function
+			elif currentFunction == enums.Function.Figure: # figure function
 				Figure(data)
 
 

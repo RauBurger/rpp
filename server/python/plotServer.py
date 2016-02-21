7d4dabd108b9bef5b63713e6b3350ff808287a61
Rob Rau <rfr2@mac.com>
2016-02-21 16:34:10 -0500

    Added Plot function for python server. Needs to be expanded

--- server/python/plotServer.py
+++ server/python/plotServer.py
@@ -3,9 +3,55 @@
 import matplotlib.pyplot as plt
 import socket
 import enums 
+import struct
 
 def Plot(data):
-	print('butts')
+	print(type(data))
+
+	fmt = data[1]
+	lines = data[2]
+
+	dataOffset = 3;
+	for idx in range(lines):
+
+		lineLen = int(int.from_bytes(data[dataOffset:dataOffset+4], 'little', signed=False)/8)
+		print("lineLen = "+str(lineLen))
+
+		dataOffset += 4
+
+		x = []
+		y = []
+
+		for j in range(lineLen):
+			x.append(struct.unpack('<d', data[dataOffset:dataOffset+8])[0])
+			dataOffset += 8
+
+		for j in range(lineLen):
+			y.append(struct.unpack('<d', data[dataOffset:dataOffset+8])[0])
+			dataOffset += 8
+
+		fmtStr = ''
+		if fmt == 1:
+			fmtLen = data[dataOffset]
+			dataOffset += 1
+			
+			fmtStr = data[dataOffset:dataOffset+fmtLen].decode()
+			dataOffset += fmtLen
+
+			print(fmtStr)
+
+			plt.hold(True)
+
+			plt.plot(x, y, fmtStr)
+			plt.show(block = False)
+		else:
+			plt.hold(True)
+
+			plt.plot(x, y)
+			plt.show(block = False)
+
+	plt.hold(False)
+
 
 def Figure(data):
 	print('in figure')
@@ -30,29 +76,41 @@ def serve():
 		# get data from client
 		data = client.recv(currentPayload)
 		
-		print('got data')
+		#print('got data')
 		
 		# acknowledge client
-		print('len(data) = '+str(len(data)))
-		client.send(b'\x00'+int.to_bytes(4, len(data), 'little', signed=False))
+		#print('len(data) = '+str(len(data)))
+		#print('data[0] = '+str(data[0]))
+		#intBytes = int.to_bytes(4, len(data), 'little', signed=False)
+		intBytes = struct.pack('<I', len(data))
+		#print('intBytes = '+str(intBytes))
+
+		print("currentPayload = "+str(currentPayload))
+		clientData = b'\x00'+intBytes
+
+		client.send(clientData)
 
 		currentCommand = data[0]
-		print('currentCommand = '+str(currentCommand))
+		#print('currentCommand = '+str(currentCommand))
 		if currentCommand == enums.Command.Function: # function command
 			currentFunction = data[1]
 			currentPayload = int.from_bytes(data[2:-1], 'little', signed=False)
-			print('currentFunction = '+str(currentFunction))
-			print('currentPayload = '+str(currentPayload))
+			#print('currentFunction = '+str(currentFunction))
+			#print('currentPayload = '+str(currentPayload))
 
 		elif currentCommand == enums.Command.Data: # data command
+			currentPayload = 1
 			if currentFunction == enums.Function.Plot: # plot function
 				Plot(data)
 			elif currentFunction == enums.Function.Figure: # figure function
 				Figure(data)
 
+		elif currentCommand == enums.Command.Done:
+			currentPayload = 10
 
-
-		#print(data)
+		elif currentCommand == enums.Command.Close:
+			plt.show()
+		
 
 
 if __name__ == '__main__':
